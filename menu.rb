da6b730624bab73b8ca86c48ea2bbbbc5383dbf5
Artemiy Mutovin <artemmutovin@mail.ru>
2022-10-26 06:47:49 -0400

    изменения в меню игры

--- menu.rb
+++ menu.rb
@@ -5,10 +5,15 @@ require_relative 'card'
 require_relative 'game'
 require_relative 'user'
 
-# puts 'Введите ваше имя'
-# name = gets.chomp
 
-# player = Player.new(name)
-# dealer = Dealer.new
+puts 'Введите ваше имя'
+name = gets.chomp
 
-# puts "Привет #{player.name}, начнем игру!"
+player = Player.new(name)
+dealer = Dealer.new
+
+puts "Привет #{player.name}, начнем игру!"
+
+Game.new(player, dealer)
+
+puts "Ваши карты: #{player.cards}"
